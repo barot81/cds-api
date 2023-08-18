e21da509ac01d86590a5bc84ef12ec2e45334f9b
Ankit Kapatel <ankit.kapatel@exxat.com>
2023-08-19 02:28:39 +0530

    added nullable fields

--- Zhealthcare.Service/Domain/Entities/PatientFinding.cs
+++ Zhealthcare.Service/Domain/Entities/PatientFinding.cs
@@ -12,21 +12,21 @@
         public string ClinicalIndicator { get; set; } = string.Empty;
         public string CurrentDrgNo { get; set; } = string.Empty; 
         public string CurrentDrgDescription { get; set; } = string.Empty; 
-        public double InitialWeight { get; set; }
-        public double Gmlos { get; set; }
+        public double? InitialWeight { get; set; }
+        public double? Gmlos { get; set; }
         public string ExpectedDrgNo { get; set; } = string.Empty;
         public string ExpectedDrgDescription { get; set; } = string.Empty;
-        public double ExpectedWeight { get; set; }
-        public double ExpectedGmlos { get; set; }
+        public double? ExpectedWeight { get; set; }
+        public double? ExpectedGmlos { get; set; }
         public DateTime? ResponseDate { get; set; }
         public string ResponseType { get; set; } = string.Empty;
         public string ResponseComment { get; set; } = string.Empty;
         public string FollowupComment { get; set; } = string.Empty;
         public string RevisedDrgNo { get; set; } = string.Empty;
         public string RevisedDrgDescription { get; set; } = string.Empty;
-        public double RevisedWeight { get; set; }
-        public double RevisedGmlos { get; set; }
-        public double WeightDifference { get; set; }
+        public double? RevisedWeight { get; set; }
+        public double? RevisedGmlos { get; set; }
+        public double? WeightDifference { get; set; }
         public string QueryStatus { get; set; } = string.Empty;
         public string ClinicalSummary { get; set; } = string.Empty;
         public string Comments { get; set; } = string.Empty;
